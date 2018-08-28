59e25076c76c541eb916b09e373da3080dc4bd84
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-28 14:36:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,25 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get "/" do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+      #erb :index
+    end
+
+  get "/new" do
+    erb :"pirates/new"
+  end
+
+  post "/pirates" do
+    @pirate =  Pirate.new(params[:pirate])
+    #binding.pry
+    params[:pirate][:ships].each do |details|
+      Ship.new(details)
+    end
+    @ship = Ship.all
+    erb :"pirates/show"
+    #binding.pry
+  end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_reader :name, :weight, :height
+  PIRATES = []
+  def initialize(arg)
+    @name = arg[:name]
+    @weight = arg[:weight]
+    @height = arg[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+   
 end
