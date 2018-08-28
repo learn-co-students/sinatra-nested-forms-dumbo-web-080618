fd2f47f182d19d9b7c1180aa434b19d2bc96870d
github username <github email address>
2018-08-28 17:48:00 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+
+  PIRATES = []
+  def initialize(args)
+    @name = args[:name]
+    @height = args[:height]
+    @weight = arg[:weight]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
+  def self.clear
+    PIRATES.clear
+  end  
+
 end
