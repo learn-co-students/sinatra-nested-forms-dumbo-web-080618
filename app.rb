6d153c6ab358bfab0a13b3b571e8532ea0b3daed
github username <github email address>
2018-08-28 23:05:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get "/" do
+      "Welcome to the Nested Forms Lab!let's navigate to the '/new'"
 
+    end
+    get "/new" do
+
+     erb :"pirates/new"
+    end
+
+    post "/pirates" do
+      @np = Pirate.new(params[:pirate]) #made a new instance of pirate
+      @sh = params[:pirate][:ships].each{|boat| Ship.new(boat) } #boat is the hash with name type booty and its values, so boat is the hash with those attributes
+      @all = Ship.all #see all the  new ships
+      erb :"/pirates/show"
+
+    end
     # code other routes/actions here
-
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_reader :name, :type , :booty
+    SHIPS = []
+    def initialize(blondy)
+      @name = blondy[:name]
+      @type = blondy[:type]
+      @booty = blondy[:booty]
+      SHIPS << self
+    end
+    def self.all
+      SHIPS
+    end
+    def self.clear
+      SHIPS.clear
+    end
 end
